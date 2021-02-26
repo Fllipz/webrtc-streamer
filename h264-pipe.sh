e353ce90ea82bd8c65c9b79115ca51c9f2237832
KonradPR <konrad.przewloka@gmail.com>
2021-02-26 07:11:48 +0000

    Lower latnecy on h264 codec

--- h264-pipe.sh
+++ h264-pipe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ffmpeg -f v4l2 -framerate 10 -video_size 320x240 -codec:v h264 -i /dev/video0 \
-         -codec:v libx264 -profile:v baseline -preset ultrafast -tune zerolatency -s 320x240 -b:v 1000k -f rtp rtp://localhost:8005 
+ffmpeg -f v4l2 -framerate 15 -video_size 320x240 -codec:v h264 -i /dev/video0 \
+         -an -c:v copy  -f rtp rtp://localhost:8005 
