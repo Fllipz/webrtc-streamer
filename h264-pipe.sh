9ac1cc51db00e0985196a117425b8ac42f1f3d6e
Konrad <konrad.przewloka@gmail.com>
2021-03-10 14:30:20 +0100

    Removed unnesessary pipelines scripts

--- h264-pipe.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ffmpeg -f v4l2 -framerate 15 -video_size 320x240 -codec:v h264 -i /dev/video0 \
-         -an -c:v copy  -f rtp rtp://localhost:8005 
--- mock-h264-pipe.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-ffmpeg -re -f lavfi  -i testsrc=size=1280x720:rate=30 -c:v libx264 -b:v 1600k -preset ultrafast  -tune zerolatency   -b 1000k -f rtp rtp://localhost:8005
